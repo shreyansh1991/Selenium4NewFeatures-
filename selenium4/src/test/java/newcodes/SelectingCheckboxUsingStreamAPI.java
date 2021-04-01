3bdf69d9ceb10c29fadbebe61b4ae9137dfe34c5
Shreyansh Jain <shreyansh791@yahoo.in>
2021-04-01 08:57:08 +0530

    java8 code added

---
+++ selenium4/src/test/java/newcodes/SelectingCheckboxUsingStreamAPI.java
@@ -0,0 +1,26 @@
+package newcodes;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SelectingCheckboxUsingStreamAPI {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://vins-udemy.s3.amazonaws.com/java/html/java8-stream-table.html");
+	AtomicInteger counter = new AtomicInteger();
+	driver.findElements(By.xpath("//tbody/tr"))
+    .stream()
+    .peek(i->counter.incrementAndGet())
+    .skip(1)
+    .filter(webElement -> webElement.getText().contains("Male"))
+    .map(e -> e.findElement(By.xpath("//tr["+counter+"]//input[@type='checkbox']")))
+    .forEach(e->e.click());
+    
+}
+}
